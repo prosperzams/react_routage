51d29e34c77ad099482aba986df8c21f3165b2f4
prosperzams <prosperzams@gmail.com>
2025-02-20 18:58:32 +0100

    prosper

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import App from './App.jsx'
+import Home from './view/Home.jsx'
+import About from './view/About.jsx'
+import Contact from './view/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
